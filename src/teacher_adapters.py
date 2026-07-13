68315c7b898bcf7dec33ef32d51ef82cb8ae32e4
Codevidamme <hwiyou2005@gmail.com>
2026-07-14 03:31:11 +0700

    Use default initialization for adapter down projection

--- src/teacher_adapters.py
+++ src/teacher_adapters.py
@@ -10,7 +10,7 @@ class ResidualAdapter(nn.Module):
         self.norm = nn.LayerNorm(feature_dim)
         self.down = nn.Linear(feature_dim, bottleneck_dim)
         self.up = nn.Linear(bottleneck_dim, feature_dim)
-        nn.init.xavier_uniform_(self.down.weight)
+        # Ablation: keep PyTorch's default initialization for down.weight.
         nn.init.zeros_(self.down.bias)
         nn.init.zeros_(self.up.weight)
         nn.init.zeros_(self.up.bias)
