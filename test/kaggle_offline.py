79c61315bf1e668dad9d5f6d39230624c45c9139
Codevidamme <hwiyou2005@gmail.com>
2026-09-16 20:22:50 +0700

    Pin cache reuse fix in Kaggle setup

--- test/kaggle_offline.py
+++ test/kaggle_offline.py
@@ -12,7 +12,7 @@ from pathlib import Path
 
 EXPECTED_REPOSITORY = "https://github.com/HieuNM1804/KD-SBIR.git"
 EXPECTED_BRANCH = "experiment/sgcd-native-prompt-learning"
-EXPECTED_COMMIT = "396563030147c0bc7d20f4abacf7effd451f78d2"
+EXPECTED_COMMIT = "fe01afc2f51f5350b9bad53cffe40baed657ddf7"
 EXPECTED_TASK = "sgcd_native_prompt_learning"
 EXPECTED_ENTRYPOINT = "src.train"
 EXPECTED_DATASET = "b20dccn616nguynhutun/sketchy"
--- test/kaggle_online.py
+++ test/kaggle_online.py
@@ -26,7 +26,7 @@ DFN_DIR = BUNDLE / "dfn5b_openclip"
 REPO_URL = "https://github.com/HieuNM1804/KD-SBIR.git"
 BRANCH = "experiment/sgcd-native-prompt-learning"
 # Pinned training source; this builder is distributed separately.
-COMMIT = "396563030147c0bc7d20f4abacf7effd451f78d2"
+COMMIT = "fe01afc2f51f5350b9bad53cffe40baed657ddf7"
 TASK = "sgcd_native_prompt_learning"
 ENTRYPOINT = "src.train"
 DATASET = "b20dccn616nguynhutun/sketchy"
