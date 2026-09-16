17a9f9eb8aa473a5923e17242341d9275e44518c
Codevidamme <hwiyou2005@gmail.com>
2026-09-17 02:41:27 +0700

    Pin SGCD control contract v2 source

--- test/kaggle_offline.py
+++ test/kaggle_offline.py
@@ -12,7 +12,7 @@ from pathlib import Path
 
 EXPECTED_REPOSITORY = "https://github.com/HieuNM1804/KD-SBIR.git"
 EXPECTED_BRANCH = "experiment/sgcd-native-prompt-learning"
-EXPECTED_COMMIT = "7871127f381d2ff3c088da482ddad3a78273b979"
+EXPECTED_COMMIT = "50e559fa5650470f31d9257b212fa7bd270f7d6b"
 EXPECTED_TASK = "sgcd_native_prompt_learning"
 EXPECTED_ENTRYPOINT = "src.train"
 EXPECTED_DATASET = "b20dccn616nguynhutun/sketchy"
