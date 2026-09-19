1e5bc31e30342f77007d1964421736094b0d02d2
Codevidamme <hwiyou2005@gmail.com>
2026-09-19 21:21:57 +0700

    Repin seed-42 Gap-CoRe sensitivity sweep

--- test/kaggle_gap_core_online.py
+++ test/kaggle_gap_core_online.py
@@ -26,7 +26,7 @@ DFN_DIR = BUNDLE / "dfn5b_openclip"
 
 REPOSITORY = "https://github.com/HieuNM1804/KD-SBIR.git"
 BRANCH = "experiment/gap-core-teacher-audit"
-COMMIT = "082997f7850384899d68a2c53a4244b7ef138234"
+COMMIT = "5975d0cbc8add11221cf1b0e6b3d3daeedab3967"
 TASK = "gap_core_distillation"
 ENTRYPOINT = "src.train"
 DATASET = "b20dccn616nguynhutun/sketchy"
